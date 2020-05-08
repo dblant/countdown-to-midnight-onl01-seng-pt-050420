97ee34a061d07b201ce0a03b3ed5af519727e443
Dustin Blanton <dustinblntn@gmail.com>
2020-05-08 21:22:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,13 @@ def countdown(counter)
   "HAPPY NEW YEAR!"
 end
 
+
+def countdown_with_sleep(counter)
+   
+  
+    while counter > 0 
+    puts "#{counter} SECOND(S)!"
+    counter -=1 
+  end
+  "HAPPY NEW YEAR!"
+end
