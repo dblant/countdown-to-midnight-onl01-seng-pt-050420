40f25c65de73cef4d8d60b2a8478377352a8e643
Dustin Blanton <dustinblntn@gmail.com>
2020-05-08 21:14:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,14 @@
 #write your code here
 
 def countdown(counter)
-   counter = 10
-  loop do
+   
+  
     while counter > 0 
     puts "#{counter}"
     counter -=1 
   end
   "HAPPY NEW YEAR!"
- end
+ 
   
   
 end
